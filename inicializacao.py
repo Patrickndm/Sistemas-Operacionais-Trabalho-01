809248adb67092165f45b806da6ffb0efe127118
Patrick Nelson de Morais <nelsondemoraispatrick@gmail.com>
2026-05-18 21:02:28 -0300

    Melhora da estrutura da classe Simulador e Reformulacao da ler_arquivo

--- inicializacao.py
+++ inicializacao.py
@@ -1,78 +1,89 @@
 import sys
 
-class Informacoes_Arquivo_Entrada:
-    def __init__(self, numero_processos, pids_liberados):
-        self.numero_processos = int(numero_processos)
-        self.pids_liberados = list(pids_liberados)
+class Entrada:
+    def __init__(self, numero_pids, pids):
+        self.num_pids = int(numero_pids)
+        self.pids = list(pids)
 
-class Lista_Processos:
-    def __init__(self, tipo, pid, ua=None, end_log=None):
-        self.tipo = tipo
-        self.pid = pid
-        self.ua = ua
-        self.end_log = end_log
+class Bloco_Processo:
+    def __init__(self, tipo, pid):
+        self.tipo = str(tipo)
+        self.pid = str(pid)
+        self.ua = None
+        self.end_log = None
+
+    def alocar(self, ua):
+        self.ua = int(ua)
+    
+    def acessar(self, end_log):
+        self.end_log = int(end_log)
 
 class Bloco_RAM:
-    def _initi_(self):
+    def __init__(self):
         self.alocado = False
+        self.pid = None
         
-    def alocar(self):
+    def alocar(self, pid):
         self.alocado = True
+        self.pid = pid
 
     def desalocar(self):
         self.alocado = False
+        self.pid = None
 
     def __repr__(self):
         return "Alocado" if self.alocado else "Desalocado"
     
 class Bloco_Tabela:
-    def _init(self):
-        self.pid = self.pid
-        self.end_log = self.end_log
-        self.limite = self.limite
+    def __init__(self):
+        self.pid = None
+        self.base = None
+        self.limite = None
 
     def informar_pid(self, pid):
-        self.pid = pid
+        self.pid = str(pid)
     
-    def informar_end_log(self, end_log):
-        self.pid = end_log
+    def informar_base(self, base):
+        self.base = int(base)
     
     def informar_limite(self, limite):
-        self.limite = limite
+        self.limite = int(limite)
     
 class Simulador:
-    def _init_(self, numero_processos):
+    def __init__(self, processos, entrada):
+        self.Entrada = entrada
+        
+        self.Lista_de_Processos = processos
+        
         self.RAM = []
         for _ in range(4096):
             self.RAM.append(Bloco_RAM())
 
-        self.Tabela = []
-        for _ in range(numero_processos):
-            self.RAM.append(Bloco_Tabela())
+        self.Tabela_de_Particoes = []
+        for _ in range(entrada.num_pids):
+            self.Tabela_de_Particoes.append(Bloco_Tabela())
 
 def ler_arquivo(caminho_entrada):         
     
     arquivo = open(caminho_entrada, "r")            
     
-    numero_processos = int(arquivo.readline().strip())
-    pids = arquivo.readline().strip().split(";")
-    informacoes_arquivo_entrada = [Informacoes_Arquivo_Entrada(numero_processos, pids)]
+    entrada = Entrada(int(arquivo.readline().strip()), arquivo.readline().strip().split(";"))
     
-    lista_de_processos = []
+    processos = []
     for linha in arquivo:
         parte = linha.strip().split(" ")
-        processo = Lista_Processos(parte[0], parte[1]) 
+        processo = Bloco_Processo(parte[0], str(parte[1])) 
 
         if processo.tipo == "aloca":
-            processo.ua = parte[2]
+            processo.alocar(parte[2])
         elif processo.tipo == "acessa":
-            processo.end_log = parte[2]
-
-        lista_de_processos.append(processo)
-
+            processo.acessar(parte[2])
+        processos.append(processo)
+    
+    simulador = Simulador(processos, entrada)
     arquivo.close()
-    return processos, lista_de_processos, informacoes_arquivo_entrada
-
+    
+    return simulador
 
 
 # Como invocar pelo terminar: python leitura.py [algoritmo (first | best | worst | buddy)] [caminho do arquivo]
@@ -82,11 +93,10 @@ if __name__ == "__main__":
     algoritmo = sys.argv[1]
     caminho_entrada = sys.argv[2]
     
-    caminho_entrada_refinado = caminho_entrada.replace("./exemplos_entrada/", "").replace(".txt", "")
-    caminho_saida = f"log_{caminho_entrada_refinado}_{algoritmo}.txt"
+    simulador = ler_arquivo(caminho_entrada)
     
-    processos, lista_de_processos, Informacoes_Arquivo_Entrada = ler_arquivo(caminho_entrada)
-    
-    #linha, tarefas = simular(tarefas, algoritmo)
+    #simulador = iniciar_simulacao(processos, entrada)
 
+    #caminho_entrada_refinado = caminho_entrada.replace("./exemplos_entrada/", "").replace(".txt", "")
+    #caminho_saida = f"log_{caminho_entrada_refinado}_{algoritmo}.txt"
     #salvar_saida(linha, tarefas, caminho_saida)
